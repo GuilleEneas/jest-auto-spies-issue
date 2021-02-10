0a55f63cfc89474b62990777d1c705fc571d777a
Guillermo Timón <guillermo.timon@buildingminds.com>
2021-02-10 12:09:12 +0100

    initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jest-auto-spies-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+}
